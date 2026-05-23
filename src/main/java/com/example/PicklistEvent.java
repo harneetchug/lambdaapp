3c755a2e2c391db8fd36bae107805b3e48e8cf0b
harneetchug <harneetchugh.cec@gmail.com>
2026-05-23 21:21:36 +0200

    Updated handler code

---
+++ src/main/java/com/example/PicklistEvent.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class PicklistEvent {
+
+    public String eventType;
+    public String reservationId;
+    public String interactionId;
+    public String channel;
+    public String createdAt;
+
+    public PicklistEvent() {
+    }
+}
